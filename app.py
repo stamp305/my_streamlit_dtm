e71cdcb59a0e26d81adba1c2a0f9ce4b084027f0
stamp305 <puripaht2016@gmail.com>
2025-04-20 11:51:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import numpy as np
+import pickle
 
+with open('dtm_trained_model.pkl','rb') as f:
+    dtm_model = pickle.load(f)
 # Set the title
 st.title("Iris Flower Feature Selector")
+st.write("Enter the feature of the iris flower")
 
 # Create sliders for each flower feature
 sepal_length = st.slider("Sepal Length (cm)", min_value=4.0, max_value=8.0, value=5.1)
@@ -10,8 +15,8 @@ petal_length = st.slider("Petal Length (cm)", min_value=1.0, max_value=7.0, valu
 petal_width = st.slider("Petal Width (cm)", min_value=0.1, max_value=2.5, value=0.2)
 
 # Show selected values
-st.write("### Selected Feature Values:")
-st.write(f"Sepal Length: {sepal_length} cm")
-st.write(f"Sepal Width: {sepal_width} cm")
-st.write(f"Petal Length: {petal_length} cm")
-st.write(f"Petal Width: {petal_width} cm")
+if st.button("Predict"):
+    input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = dtm_model.predict(input_data)
+    speicies = ['Setosa', 'Versicolor', 'Virginica']
+    st.success(f"The Predicted Species is: **{speicies[prediction[0]]}**")
